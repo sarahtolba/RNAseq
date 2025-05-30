a2c6f39e3cd4194d4626becac5baddaf352798c4
Sarah <sarahtolba842@gmail.com>
2025-05-31 01:05:09 +0300

    setup_mouse_data.sh

--- setup_mouse_data.sh
+++ setup_mouse_data.sh
@@ -3,26 +3,32 @@
 # Step 1: Create directories
 mkdir -p reference
 mkdir -p fastq
+mkdir -p GTF
 
 # Step 2: Change to reference directory and download the genome fasta
 cd reference
 echo "Downloading Mus musculus genome reference (GRCm39, Ensembl)"
 wget ftp://ftp.ensembl.org/pub/release-111/fasta/mus_musculus/dna/Mus_musculus.GRCm39.dna.primary_assembly.fa.gz
 
-# Step 3: Go back and change to fastq directory
+ #step3 : change to GTF directory and download it 
+cd ../GTF
+echo "Downloading Mus musculus genome GTF file"
+wget ftp://ftp.ensembl.org/pub/release-106/gtf/homo_sapiens/Homo_sapiens.GRCh38.106.gtf.gz
+ 
+# Step 4: Go back and change to fastq directory
 cd ../fastq
 
-# Step 4: Install SRA Toolkit
+# Step 5: Install SRA Toolkit
 echo "Updating system and installing SRA Toolkit..."
 sudo apt update
 sudo apt install -y sra-toolkit
 
 
-# Step 5: Download SRA data using prefetch
+# Step 6: Download SRA data using prefetch
 echo "Downloading SRA file SRR27700562..."
 prefetch SRR27700562
 
-# Step 6: Convert SRA to gzipped FASTQ
+# Step 7: Convert SRA to gzipped FASTQ
 echo "Converting SRA to gzipped FASTQ..."
 fastq-dump --split-files --gzip ~/ncbi/public/sra/SRR27700562.sra
 
